c3cebc9144a3ecc97403f08c1691b373ca370751
Alex Villablanca <1alexvillablanca@gmail.com>
2019-06-27 18:37:41 -0700

    QR Code Screen

--- screens/QRScreen.js
+++ screens/QRScreen.js
@@ -1,27 +1,83 @@
-import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
-import { ExpoLinksView } from '@expo/samples';
+import * as React from 'react';
+import { Text, View, StyleSheet, Button,Alert } from 'react-native';
+import Constants from 'expo-constants';
+import * as Permissions from 'expo-permissions';
+import { BarCodeScanner } from 'expo-barcode-scanner';
+import {withNavigation} from 'react-navigation';
+import {postJSON} from '../utils';
 
-export default function LinksScreen() {
-  return (
-    <ScrollView style={styles.container}>
-      {/**
-       * Go ahead and delete ExpoLinksView and replace it with your content;
-       * we just wanted to provide you with some helpful links.
-       */}
-      <ExpoLinksView />
-    </ScrollView>
-  );
+class QRScreen extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      hasCameraPermission: null,
+      scanned: false
+    };
+
+  }
+
+  async componentDidMount() {
+    this.getPermissionsAsync();
+    const { navigation } = this.props;
+    this.focusListener = navigation.addListener("didFocus", () => {
+      this.setState({scanned:false});
+    });
+  }
+  componentWillUnmount() {
+    this.focusListener.remove();
+  }
+
+
+  getPermissionsAsync = async () => {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    this.setState({ hasCameraPermission: status === 'granted' });
+  };
+
+  render() {
+    const { hasCameraPermission, scanned } = this.state;
+
+    if (hasCameraPermission === null) {
+      return <Text>Requesting for camera permission</Text>;
+    }
+    if (hasCameraPermission === false) {
+      return <Text>No access to camera</Text>;
+    }
+    return (
+      <View style={styles.flex}>
+        <View
+        style={styles.box1}>
+          <BarCodeScanner
+            onBarCodeScanned={scanned ? undefined:this.handleBarCodeScanned}
+            style={StyleSheet.absoluteFillObject}
+          />
+        </View>
+      </View>
+    );
+  }
+
+  handleBarCodeScanned = ({ type, data }) => {
+    this.setState({scanned: true});
+    postJSON(`/3.0/authenticated/attendee-info-by-qrcode/${data}`,{},true).then(res=>{
+      if(res.success){
+        console.log(res.data);
+      }else{
+        Alert.alert('Error',res.error,[{text: 'OK', onPress: () => this.setState({scanned:false})}]);
+      }
+    });
+  };
 }
-
-LinksScreen.navigationOptions = {
-  title: 'Links',
-};
-
 const styles = StyleSheet.create({
-  container: {
+  flex:{
     flex: 1,
-    paddingTop: 15,
-    backgroundColor: '#fff',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
+  box1: {
+    width: '75%',
+    paddingBottom: '75%',
+  },
+
 });
+
+export default withNavigation(QRScreen);
